0b494500836fa839fca42933e613d99858836435
Timo Wang <chiuanw@yahoo-inc.com>
2019-01-10 22:19:28 +0800

    list rendering

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inventory: 10
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
     }
 });
