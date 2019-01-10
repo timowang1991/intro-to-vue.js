4c9485526bf6a63a7725049d0b43eb897a9dbed5
Timo Wang <chiuanw@yahoo-inc.com>
2019-01-10 22:36:38 +0800

    mouseover event handling binding

--- main.js
+++ main.js
@@ -8,18 +8,23 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
             },
             {
                 varientId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
         cart: 0
     },
     methods: {
-        addToCart: function () {
+        addToCart() {
             this.cart += 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
         }
     }
 });
