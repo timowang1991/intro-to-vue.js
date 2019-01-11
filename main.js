3989fbac9f78c35ee93078001bbf54004b5a7116
Timo Wang <chiuanw@yahoo-inc.com>
2019-01-11 21:54:23 +0800

    add computed property inStock

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ var app = new Vue({
     data: {
         brand: 'Vue Mastery',
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: false,
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './assets/vmSocks-green-onWhite.jpg'
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 varientId: 2235,
                 variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg'
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -24,13 +25,19 @@ var app = new Vue({
         addToCart() {
             this.cart += 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
         }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
 });
